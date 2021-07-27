4fbc433cab0e3058bb7b87d1092bfa97f383dde0
Maria Villascusa <meri.villa14@gmail.com>
2021-07-27 16:02:46 +0200

    init commit

---
+++ part1/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+
+  const mensaje = 'adios'
+
+  return (
+    <div className="App">
+      <h2>hello baby</h2>
+      <h2>{mensaje + ' amigos'}</h2>
+    </div>
+  );
+}
+
+export default App;
