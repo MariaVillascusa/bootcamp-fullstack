db999fa43427d3a4c1d2b1c03b4c1ad918c5a319
Maria Villascusa <meri.villa14@gmail.com>
2021-07-27 19:31:31 +0200

    delet

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,12 +3,9 @@ import './App.css';
 
 function App() {
 
-  const mensaje = 'adios'
-
   return (
     <div className="App">
-      <h2>hello baby</h2>
-      <h2>{mensaje + ' amigos'}</h2>
+      <h1>Hello world</h1>
     </div>
   );
 }
