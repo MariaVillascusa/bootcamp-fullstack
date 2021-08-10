37fd267aea48d97673f182fc3ccf3b9c14b3470f
Maria Villascusa <meri.villa14@gmail.com>
2021-08-11 01:05:42 +0200

    2.20 phonebook step12

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -4,6 +4,7 @@ import Form from './Form'
 import Filter from './Filter'
 import personService from '../services/persons'
 import Notification from './Notification'
+import Error from './Error'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,14 +12,18 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [findValue, setFind] = useState('')
   const [message, setMessage] = useState(null)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
+    setError(null)
     personService.getAll()
       .then(initialPersons => setPersons(initialPersons))
+      .catch(error => showErrorMessage('Something went wrong'))
   }, [])
 
   const addPerson = e => {
     e.preventDefault()
+    setError(null)
 
     const personObj = {
       name: newName,
@@ -32,7 +37,7 @@ const App = () => {
             setPersons(persons.map(person => person.id !== updatedPerson[0].id ? person : changedPerson))
             clearForm()
             showMessage("Updated", personObj)
-          })
+          }).catch(err => showErrorMessage(`${updatedPerson[0].name} can not be updated`))
       } else {
         clearForm()
       }
@@ -44,8 +49,21 @@ const App = () => {
         setPersons(persons.concat(returnedPerson))
         clearForm()
         showMessage("Add", personObj)
+      }).catch(err => showErrorMessage(`${personObj.name} can not be created`))
+  }
 
-      })
+  const deletePerson = (id) => {
+    setError(null)
+    const person = persons.filter(person => person.id === id)
+    if (window.confirm(`Delete ${person[0].name} ?`)) {
+      personService
+        .deletePersons(id)
+        .then(deletedPerson => {
+          setPersons(persons.filter(p => p.id !== id))
+        }).catch(err => showErrorMessage(
+          `${person[0].name} was already removed from server`,
+        ))
+    }
   }
 
   const handleNameChange = e => setNewName(e.target.value)
@@ -62,16 +80,7 @@ const App = () => {
     const search = persons.filter(person => person.name.toLowerCase().includes(findValue.toLocaleLowerCase()))
     setPersons(search)
   }
-  const deletePerson = (id) => {
-    const person = persons.filter(person => person.id === id)
-    if (window.confirm(`Delete ${person[0].name} ?`)) {
-      personService
-        .deletePersons(id)
-        .then(deletedPerson => {
-          setPersons(persons.filter(p => p.id !== id))
-        })
-    }
-  }
+  
   const showMessage = (message, person) => {
     setMessage(`${message} ${person.name}`)
     setTimeout(() => {
@@ -79,11 +88,19 @@ const App = () => {
     }, 2000)
   }
 
+  const showErrorMessage = (errorMessage) => {
+    setError(errorMessage)
+    setTimeout(() => {
+      setError(null)
+    }, 7000)
+  }
+
   return (
     <div className="container">
       <h1>Phonebook</h1>
-      <Filter findValue={findValue} findPerson={findPerson} />
       <Notification message={message} />
+      <Error error={error} />
+      <Filter findValue={findValue} findPerson={findPerson} />
       <h2>Add a new</h2>
       <Form onSubmit={addPerson} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
